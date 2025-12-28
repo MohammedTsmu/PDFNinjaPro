7bb279c801a22a976fbe33126373920313f9d606
MohammedTsmu <mohsat69@yahoo.com>
2025-12-29 02:31:08 +0300

    update copyright

--- js/copyright.js
+++ js/copyright.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const currentYear = new Date().getFullYear();
-    const companyName = 'Mohammed Q. Sattar Production';
-    const encodedCompanyName = btoa(companyName);
-    const encodedYear = btoa(currentYear.toString());
-    const copyrightText = `&copy;2024- ${atob(encodedYear)} ${atob(encodedCompanyName)}. All rights reserved.`;
-
+    const copyrightText = `© ${currentYear} PDF Ninja Pro. All rights reserved.`;
     document.getElementById('copyright').innerHTML = copyrightText;
 });
